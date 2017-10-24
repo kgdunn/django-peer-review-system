77300a44e83412a13ff2d378f535d821e4b3e137
Kevin Dunn <kgdunn@gmail.com>
2017-10-24 14:01:57 +0200

    Testing the calling of a new entry point

--- keyterms/views.py
+++ keyterms/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.clickjacking import xframe_options_exempt
 
-def test(request):
-    HttpResponse(content='Hi there')
+# Logging
+import logging
+logger = logging.getLogger(__name__)
+
+# Debugging
+import wingdbstub
+
+#@csrf_exempt
+#@xframe_options_exempt
+def test(request, course=None, learner=None, entry_point=None):
+    """
+    Start the interactive tool here:
+    0. Can we even run this entry_point?
+    """
+    return HttpResponse(content='Hi there')
+
