20df1e359f245f93a0514f0044779ffd65425994
Kevin Dunn <kgdunn@gmail.com>
2017-10-25 15:04:38 +0200

    Workflow now mostly working

--- keyterms/urls.py
+++ keyterms/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from basic import views
 
 urlpatterns = [
-    url(r'^$', views.starting_point, name='starting_point'),
-    url(r'^draft/$', views.draft_keyterm, name='draft_keyterm'),
-    url(r'^preview/$', views.preview_keyterm, name='preview_keyterm'),
-    url(r'^submit/$', views.submit_keyterm, name='submit_keyterm'),
-    url(r'^final/$', views.final_keyterms, name='view_all_keyterms'),
+    url(r'^$', views.entry_point_discovery, name='keyterms_entry_point'),
+    #url(r'^draft/$', views.draft_keyterm, name='draft_keyterm'),
+    #url(r'^preview/$', views.preview_keyterm, name='preview_keyterm'),
+    #url(r'^submit/$', views.submit_keyterm, name='submit_keyterm'),
+    #url(r'^final/$', views.final_keyterms, name='view_all_keyterms'),
 ]
--- keyterms/views.py
+++ keyterms/views.py
@@ -1,69 +1,63 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from basic.views import entry_point_discovery
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.clickjacking import xframe_options_exempt
+#from django.views.decorators.csrf import csrf_exempt#
+#from django.views.decorators.clickjacking import xframe_options_exempt
 
 
 # Logging
 import logging
 logger = logging.getLogger(__name__)
 
-@csrf_exempt
-@xframe_options_exempt
-def starting_point(request):
-    return entry_point_discovery(request)
+#@csrf_exempt
+#@xframe_options_exempt
+#def starting_point(request):
+    #return entry_point_discovery(request)
 
-def draft_keyterm(request, course=None, learner=None, entry_point=None):
+def start_keyterm(request, course=None, learner=None, entry_point=None):
     """
     """
-
     logger.debug(request.POST)
-    logger.debug('Draft keyterm [{}] for {}'.format(entry_point, learner))
 
     keyterm = request.POST.get('custom_keyterm', '')
     keyterm_finalized = False
 
-    # Code here to determine if the user has finalized their keyterm
-
+    # TODO: Code here to determine if the user has finalized their keyterm
 
     if request.POST.get('preview-keyterm', ''):
-        return preview_keyterm(request,
-                               course=None,
-                               learner=None,
-                               entry_point=None)
+        return preview_keyterm(request, course, learner, entry_point, keyterm)
 
     if request.POST.get('draft-keyterm', ''):
-        return draft_keyterm(request,
-                             course=None,
-                             learner=None,
-                             entry_point=None)
+        return draft_keyterm(request, course, learner, entry_point, keyterm)
 
     if request.POST.get('submit-keyterm', ''):
-        return submit_keyterm(request,
-                             course=None,
-                             learner=None,
-                             entry_point=None)
+        return submit_keyterm(request, course, learner, entry_point, keyterm)
 
     if request.POST.get('finalize-keyterm', '') or keyterm_finalized:
-        return finalize_keyterm(request,
-                             course=None,
-                             learner=None,
-                             entry_point=None)
+        return finalize_keyterm(request, course, learner, entry_point, keyterm)
+
+    # If nothing else (usually the first time we start, we start with drafting)
+    return draft_keyterm(request, course, learner, entry_point, keyterm)
 
 
-    ctx = {'keyterm': 'TCK',
+def draft_keyterm(request, course=None, learner=None, entry_point=None,
+                  keyterm=None):
+    """
+    """
+    ctx = {'keyterm': keyterm,
            'course': course,
            'entry_point': entry_point,
            'learner': learner,
-          }
+           }
     return render(request, 'keyterms/draft.html', ctx)
 
 
-def preview_keyterm(request, course=None, learner=None, entry_point=None):
+def preview_keyterm(request, course=None, learner=None, entry_point=None,
+                    keyterm=None):
     """
     """
-    ctx = {'keyterm': 'TCK',
+
+    ctx = {'keyterm': keyterm,
            'course': course,
            'entry_point': entry_point,
            'learner': learner,
@@ -71,20 +65,22 @@ def preview_keyterm(request, course=None, learner=None, entry_point=None):
     return render(request, 'keyterms/preview.html', ctx)
 
 
-def submit_keyterm(request, course=None, learner=None, entry_point=None):
+def submit_keyterm(request, course=None, learner=None, entry_point=None,
+                   keyterm=None):
     """
     """
-    ctx = {'keyterm': 'TCK',
+    ctx = {'keyterm': keyterm,
            'course': course,
            'entry_point': entry_point,
            'learner': learner,
            }
     return render(request, 'keyterms/submit.html', ctx)
 
-def final_keyterms(request, course=None, learner=None, entry_point=None):
+def finalize_keyterm(request, course=None, learner=None, entry_point=None,
+                     keyterm=None):
     """
     """
-    ctx = {'keyterm': 'TCK',
+    ctx = {'keyterm': keyterm,
            'course': course,
            'entry_point': entry_point,
            'learner': learner,
