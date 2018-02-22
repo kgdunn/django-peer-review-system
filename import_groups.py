b271912a2d165b75277672020bd4ce7919d8318c
Kevin Dunn <kgdunn@gmail.com>
2018-02-22 16:08:38 +0100

    Import for production

--- import_groups.py
+++ import_groups.py
@@ -6,12 +6,12 @@ gfp = Group_Formation_Process.objects.get(id=2)
 import csv
 with open('course-list.csv') as csvfile:
     reader = csv.DictReader(csvfile)
-
+    #
     for row in reader:
         first = row['FirstName']
         last = row['LastName']
         display_name = '{0} {1}'.format(first, last)
-
+        #
         students = Person.objects.filter(email=row['Email'].lower())
         if students.count():
             student = students[0]
@@ -27,7 +27,7 @@ with open('course-list.csv') as csvfile:
             student.user_ID = 'Stu-{0}'.format(student.id)
             student.save()
             print('Added student: {}'.format(student) )
-
+#
         # Now enroll the student in a group
         group_name = row['GroupName']
         name = 'CE-P1-{}'.format(group_name)
@@ -37,19 +37,17 @@ with open('course-list.csv') as csvfile:
         else:
             group = Group(name=name, capacity=5, gfp=gfp)
             group.save()
-
+#
         enrolled = student.groupenrolled_set.filter(group__gfp=gfp,
                                                     is_enrolled=True)
         if enrolled.count():
             enrolled.delete()
             print('Student was ALREADY ENROLLED; deleted and about to change')
-
+#
         enrollment = GroupEnrolled(person=student,
                                    group=group,
                                    is_enrolled=True)
         enrollment.save()
-
-        print('Student: {} enrolled in group {}'.format(student,
-                                                        enrollment))
-
+        #
+        print('Student: {} enrolled in group {}'.format(student, enrollment))
 print('=====FINISHED=====')
